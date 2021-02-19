d9f8d72f543a11332e0b256ad1e5d38f049e0c9d
Sarah Osgood <sarahjs117@gmail.com>
2021-02-19 14:24:03 -0700

    Complete initial set up of Home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,15 +1,16 @@
 import './Home.scss'
 import { Link } from 'react-router-dom'
+import lady from '../assets/lady.png'
 
 const Home = (props) => {
   return(
     <section className='home'>
 
-      <h1>Pack Smart</h1>
-      <p>Welcome to Pack Smart! Let us help you get started on your next travel adventure!</p>
-      <img/>
+      <h1 className='pack-smart-title'>Pack Smart</h1>
+      <p>Welcome to Pack Smart! Let us help you get started packing for your next travel adventure!</p>
+      <img src={ lady }/>
       <Link to='/packing-quiz'>
-        <button>Get Started!</button>
+        <button className='start-button'>Get Started!</button>
       </Link>
     </section>
   )
