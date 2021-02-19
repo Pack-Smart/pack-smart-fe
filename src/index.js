89ccf24ec64c7f6a4a09e7bb517339dd439f1568
Alyssa Bull <alyssabull@gmail.com>
2021-02-19 13:36:09 -0700

    Add react router dom to dependencies

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import BrowserRouter from 'react-router-dom';
+
+const router = <BrowserRouter><App /></BrowserRouter>
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+    {router}
   </React.StrictMode>,
   document.getElementById('root')
 );
