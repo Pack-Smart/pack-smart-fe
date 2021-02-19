346d3d11481bdf116001c22ca6bcdb06ff716523
Sarah Osgood <sarahjs117@gmail.com>
2021-02-19 14:06:21 -0700

    Begin styling of Home component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,8 +1,17 @@
 import './Home.scss'
+import { Link } from 'react-router-dom'
 
 const Home = (props) => {
   return(
-    <h1>HOME</h1>
+    <section className='home'>
+
+      <h1>Pack Smart</h1>
+      <p>Welcome to Pack Smart! Let us help you get started on your next travel adventure!</p>
+      <img/>
+      <Link to='/packing-quiz'>
+        <button>Get Started!</button>
+      </Link>
+    </section>
   )
 }
 
