b7075b4c46147214d7ebe8dae72e33fed6399164
JP Carey <jpcarey4@gmail.com>
2021-02-19 14:49:51 -0700

    add initial styling for navbar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,8 +1,28 @@
 import './Header.scss'
+import { Link } from 'react-router-dom'
+import cases from './cases.png'
 
 const Header = (props) => {
   return(
-    <h1>HELLO</h1>
+    <nav className="header">
+      <Link
+        to='/'
+      >
+        <img
+          src={cases}
+          alt="pack smart logo"
+          className="header-logo" 
+        />
+      </Link>
+      <Link
+        className="header-btn-anchor"
+        to="/saved-packing-lists"
+      >
+        <button
+          className="header-btn"
+        >Saved Packing Lists</button>
+      </Link>
+    </nav>
   )
 }
 
