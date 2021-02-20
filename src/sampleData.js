efc2aad739f92cb3616c5a896b9f5b0aab6e70f1
Kara Caputo <kncaputo@gmail.com>
2021-02-20 12:08:50 -0500

    Add samplePackingList

---
+++ src/sampleData.js
@@ -0,0 +1,52 @@
+export const samplePackingList = {
+  accessories: [
+    'Belts',
+    'Hat',
+    'Purses'
+  ], 
+  beach: [
+    'Bathing Suit',
+    'Flip Flops',
+    'Sun Hat',
+    'Sunscreen',
+    'Towel',
+    'Beach Games'
+  ],
+  clothing: [
+    'Blouses/ Dress Shirts',
+    'Bras',
+    'Jeans',
+    'Pajamas',
+    'Shoes',
+    'Socks',
+    'Tank Tops',
+    'Sandals/ Flip Flops'
+  ], 
+  essentials: [
+    'Eye-glasses',
+    'Hand Sanitizer',
+    'Mask',
+    'House/Car Keys',
+    'Phone',
+    'Phone Charger',
+    'Wallet',
+    'Cash',
+    'Umbrella'
+  ], 
+  misc: [
+    'Book',
+    'Cards',
+    'Kindle',
+    'Pen/Pencil',
+    'Water Bottle',
+    'Camera',
+    'Camera Charger'
+  ],
+  nightlife: [
+    'Event tickets',
+    'Night Life Outfit'
+  ],
+  toiletries: [
+    'Beard Trimmer'
+  ]
+}
