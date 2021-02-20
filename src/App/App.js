3bc4ee64a5d572bc8021bf0f7bc43e2683f7bd4a
JP Carey <jpcarey4@gmail.com>
2021-02-19 17:46:20 -0700

    add elements for structure of quiz

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,8 @@
 import { Switch, Route } from 'react-router-dom'
 import Header from '../Header/Header.js'
-import Home from '../Home/Home.js'
-import Error from '../Error/Error.js'
+import Home from '../Home/Home'
+import Error from '../Error/Error'
+import Quiz from '../Quiz/Quiz'
 import './App.scss'
 
 const App = (props) => {
@@ -11,6 +12,7 @@ const App = (props) => {
       <main>
         <Switch>
           <Route exact path='/' component={Home}/>
+          <Route exact path='/packing-quiz' component={Quiz}/>
           <Route path='/' component={Error} />
         </Switch>
       </main>
--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -1,7 +1,61 @@
 import './Quiz.scss'
 
 const Quiz = (props) => {
-
+  return (
+    <form className="quiz-form">
+      <div className="trip-name-field">
+        <h2>Packing List Name</h2>
+        <input
+          type="text"
+          placeholder="e.g. Cancun 20 08!"
+        />
+      </div>
+      <div className="trip-gender-field">
+        <h2>I'm packing for:</h2>
+        <button>Female</button>
+        <button>Male</button>
+        <button>Non-Binary</button>
+      </div>
+      <div className="trip-location-field">
+        <h2>Going to:</h2>
+        <input
+          type="text"
+          placeholder="e.g. Mexico"
+        />
+      </div>
+      <div className="trip-duration-field">
+        <h2>Number of days</h2>
+        <input 
+          type="number"
+          placeholder="3"
+        />
+      </div>
+      <div className="trip-weather-field">
+        <h2>What will the weather be like?</h2>
+        <p>(Select all that apply)</p>
+        <button>Cold</button>
+        <button>Cool</button>
+        <button>Warm</button>
+        <button>Hot</button>
+        <button>Rainy</button>
+        <button>Snowy</button>
+      </div>
+      <div className="trip-activities-field">
+        <h2>What activities do you plan to do</h2>
+        <p>(Select all that apply)</p>
+        <button>Skiing</button>
+        <button>Beach</button>
+        <button>Hiking</button>
+        <button>Night Life</button>
+        <button>Wedding</button>
+        <button>Business</button>
+      </div>
+      <button
+        className="quiz-submit-btn"
+      >Get My Packing List!
+      </button>
+    </form>
+  )
 }
 
 export default Quiz
