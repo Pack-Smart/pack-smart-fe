ecd4e7b2727f456fa4c43a1f3266fdcd43dcc338
Alyssa Bull <alyssabull@gmail.com>
2021-02-22 14:54:40 -0700

    Delete commented out code in apiCall

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,6 @@ export const getPackingListData = (submissionData) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
-      // "Target-URL": "http://127.0.0.1:5000/api/v1/list/new"
     },
     body: JSON.stringify(submissionData)
   })
