1c1e36c982fea56441919a9db9ca20effc6fa505
Kara Caputo <kncaputo@gmail.com>
2021-02-24 20:55:14 -0500

    Add styles to center and wrap

--- src/QuestionInput/QuestionInput.js
+++ src/QuestionInput/QuestionInput.js
@@ -18,6 +18,7 @@ const QuestionInput = (props) => {
           type={type}
           placeholder={placeholder}
           name={name}
+          className='number-input'
           onChange={handleChange}
           min='1'
           onKeyDown={(evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()}
