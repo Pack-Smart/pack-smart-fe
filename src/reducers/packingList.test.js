85d5b5487be382e9c7d6e7ca4784758df7e1ea05
JP Carey <jpcarey4@gmail.com>
2021-02-25 18:56:44 -0700

    set up toggle checked reducer test

--- src/reducers/packingList.test.js
+++ src/reducers/packingList.test.js
@@ -1,4 +1,4 @@
-import { packingList } from './packingList'
+import { packingList, itemToToggle } from './packingList'
 import * as actions from '../actions/actions'
 import { sampleCategories } from '../sampleData'
 
@@ -23,5 +23,31 @@ describe('packingList', () => {
     const newAction = actions.setCurrentList(list)
     const state = packingList(initialState, newAction)
     expect(state).toEqual(list)
+  })
+})
+
+describe('packing list updates', () => {
+  beforeEach(() => {
+    const initialState = {}
+    const list = {
+      tripDetails: {
+        title: 'Cabo 2008 Betches',
+        destination: 'Mexico',
+        number_of_days: '2',
+      },
+      categories: { sampleCategories }
+    }
+    const newAction = actions.setCurrentList(list)
+    const state = packingList(initialState, newAction)
+  })
+
+  it('should toggle checked on an item', () => {
+    const initialStatus = false
+    const status = true
+    const category = 'Accessories'
+    const name = 'Scarf'
+    const newAction = actions.toggleIsChecked(category, name, status)
+    const checkedState = itemToToggle(initialStatus, status)
+    expect(checkedState).toEqual(newAction)
   })
 })
