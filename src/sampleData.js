6e08a0c5167af906e846b0eafc13cc64067f5faa
Kara Caputo <kncaputo@gmail.com>
2021-02-25 21:49:39 -0500

    Add mockState to sampleData

--- src/sampleData.js
+++ src/sampleData.js
@@ -51,3 +51,30 @@ export const sampleCategories = {
     'Lotion'
   ]
 }
+
+export const mockState = {
+  userInfo: {
+    userId: 1
+  },
+  packingList: {
+    tripDetails: {
+      destination: 'Mexico',
+      number_of_days: '7',
+      title: 'Graduation Parrrtay'
+    },
+    categories: {
+      'Accessories': [
+        {name: 'Hat', quantity: 0, is_checked: false},
+        {name: 'Belts', quantity: 1, is_checked: false},
+        {name: 'Purses', quantity: 0, is_checked: false},
+      ],
+      'Beach': [],
+      'Business': [],
+      'Child 0-2': [],
+      'Child 3-6': [],
+      'Child 7-12': [],
+      'Clothing': [],
+      'Essentials': [],
+    }
+  }
+}
