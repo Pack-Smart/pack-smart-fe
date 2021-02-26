93ff7f852bd72d4ca53a7281b5edca360c55465f
Sarah Osgood <sarahjs117@gmail.com>
2021-02-26 10:10:12 -0700

    Complete reducer/packingList unit tests for toggle and quantity

--- src/reducers/packingList.test.js
+++ src/reducers/packingList.test.js
@@ -1,4 +1,4 @@
-import { packingList, itemToToggle } from './packingList'
+import { packingList } from './packingList'
 import * as actions from '../actions/actions'
 import { mockState as categories } from '../sampleData'
 import { waitFor } from '@testing-library/react'
@@ -32,69 +32,67 @@ describe('packing list updates', () => {
   let state;
   beforeEach(() => {
     const initialState = {}
-    // const list = {
-    //   tripDetails: {
-    //     title: 'Cabo 2008 Betches',
-    //     destination: 'Mexico',
-    //     number_of_days: '2',
-    //   },
-    //   categories
-    // }
     const newAction = actions.setCurrentList(categories)
     state = packingList(initialState, newAction)
   })
 
-  it('should toggle checked on an item', async () => {
+  it('should toggle checked on an item', () => {
     const status = true
     const category = 'Accessories'
     const name = 'Purses'
 
     const sampleCheckedCategory = [
-      {name: 'Hats', quantity: 0, is_checked: false},
-      {name: 'Belts', quantity: 0, is_checked: false},
-      {name, quantity: 0, is_checked: true},
+      {name: 'Hats', quantity: 0, "item_id": 1, is_checked: false},
+      {name: 'Belts', quantity: 0, "item_id": 2,is_checked: false},
+      {name, quantity: 0, "item_id": 3, is_checked: true},
     ]
     
     const updatedCheckedState = {
-      ...state.packingList, 
+      ...state.packingList,
       categories: {
         ...state.packingList.categories,
         [category]: sampleCheckedCategory
-      }}
+      }
+    }
 
-    // expect(state).not.toEqual(updatedCheckedState)
+    expect(state).not.toEqual(updatedCheckedState)
+
+    let toggleAction = actions.toggleIsChecked(category, name, status)
+    let toggledState = packingList(state.packingList, toggleAction)
       
-    actions.toggleIsChecked(category, name, status)
-    
-    await waitFor (() => {
-      expect(state).toEqual(updatedCheckedState)
-    })
+    expect(toggledState).toEqual(updatedCheckedState)
+
   })
 
-  // it('should update the quantity on an item', () => {
-  //   const quantity = 1
-  //   const category = 'Accessories'
-  //   const name = 'Belts'
+  it('should update the quantity on an item', () => {
+    const quantity = 1
+    const category = 'Accessories'
+    const name = 'Belts'
 
-  //   const sampleQuantityCategory = [
-  //     {name: 'Hats', quantity: 0, is_checked: false},
-  //     {name: 'Belts', quantity: 1, is_checked: false},
-  //     {name: 'Purses', quantity: 0, is_checked: false},
-  //   ]
+    const sampleQuantityCategory = [
+      {name: 'Hats', quantity: 0, "item_id": 1, is_checked: false},
+      {name: 'Purses', quantity: 0, "item_id": 3, is_checked: true},
+      {name: 'Belts', quantity: 1, "item_id": 2, is_checked: false},
+    ]
 
-  //   const updatedQuantityState = {
-  //     ...state, 
-  //     categories: {
-  //       ...state.categories,
-  //     [category]: sampleQuantityCategory
-  //   }}
+    const updatedQuantityState = {
+      ...state.packingList,
+      categories: {
+        ...state.packingList.categories,
+        [category]: sampleQuantityCategory
+      }
+    }
 
-  //   expect(state).not.toEqual(updatedQuantityState)
+    expect(state).not.toEqual(updatedQuantityState)
     
-  //   actions.editItemQuantity(category, name, quantity)
+    let quantityAction = actions.editItemQuantity(category, name, quantity)
+    let quantityState = packingList(state.packingList, quantityAction)
 
-  //   expect(state).toEqual(updatedQuantityState)
-  // })
+    console.log('quantityState', quantityState.categories)
+    console.log('update', updatedQuantityState.categories)
+
+    expect(quantityState).toEqual(updatedQuantityState)
+  })
 
   // it('should delete an item', () => {
   //   const category = 'Accessories'
