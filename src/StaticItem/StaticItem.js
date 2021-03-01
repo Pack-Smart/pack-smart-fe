ebfb52a948ffb94ab2746346ac765ae9f8abab1a
Alyssa Bull <alyssabull@gmail.com>
2021-02-28 21:53:08 -0700

    Add plus and minus icons to change quantity

--- src/StaticItem/StaticItem.js
+++ src/StaticItem/StaticItem.js
@@ -2,6 +2,7 @@ import './StaticItem.scss'
 import { connect } from 'react-redux'
 import { editPackingListItem } from '../apiCalls'
 import { toggleIsChecked, deleteItem, editItemQuantity } from '../actions/actions'
+import { HiPlus, HiMinusSm } from 'react-icons/hi'
 import React, { useEffect, useState } from 'react'
 
 const StaticItem = ({ item, category, quantity, openModal, verifyDeletion, toggleIsChecked, deleteItem, editItemQuantity }) => {
@@ -34,15 +35,31 @@ const StaticItem = ({ item, category, quantity, openModal, verifyDeletion, toggl
       .catch(() => console.error)
   }
 
-  const updateItemQuantity = (event) => {
-    setItemQuantity(event.target.value)
-    updateListItem(event.target.value)
+  const updateItemQuantity = (newNum) => {
+    if (newNum >= 0) {
+      setItemQuantity(newNum)
+      updateListItem(newNum)
+    }
   }
   
   return (
     <article className='static-item'>  
       <div className='quantity-container'>
-        <input
+
+      <HiMinusSm className='plus-minus-btn' size={40} onClick={() => updateItemQuantity(itemQuantity - 1)}/>
+          <input
+            type='number'
+            name='quantity'
+            className='quantity-input'
+            aria-label='item quantity'
+            role='quantity input'
+            placeholder={quantity}
+            value={itemQuantity}
+            disabled
+          />
+        <HiPlus className='plus-minus-btn' size={40} onClick={() => updateItemQuantity(itemQuantity + 1)}/>
+
+        {/* <input
           type='number'
           name='quantity'
           className='quantity-input'
@@ -53,7 +70,7 @@ const StaticItem = ({ item, category, quantity, openModal, verifyDeletion, toggl
           onKeyDown={(evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()}
           value={itemQuantity}
           onChange={updateItemQuantity}
-        />
+        /> */}
       </div>
       <div className='item-box'>
         <p>{item.name}</p>
