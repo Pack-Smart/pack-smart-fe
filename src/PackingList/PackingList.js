c4d8b80d3683d70d35557b71e44d5e4b36c1e624
Alyssa Bull <alyssabull@gmail.com>
2021-03-02 10:33:34 -0700

    Adjust header margin on packing list

--- src/PackingList/PackingList.js
+++ src/PackingList/PackingList.js
@@ -50,26 +50,16 @@ const PackingList = ({ packingList, deleteItem, userInfo }) => {
       return (
         <>
           <header className='packing-list-header'>
-            <h2 className='packing-list-details'>{tripDetails.title} 
+            <h2 className='packing-list-title'>{tripDetails.title} 
               <BiPencil 
                 className='edit-title' 
-                size={25}
+                size={20}
                 onClick={() => setDetailsModalIsOpen(true)}
               />
             </h2>
             <p className='packing-list-details'>{tripDetails.destination} for {tripDetails.duration} {tripDetails.duration > 1 ? 'days' : 'day'}</p>
           </header>
-          <button
-            className='save-list-button-top'
-            onClick={submitNewPackingList} 
-          >Save List
-          </button>
             {createCategoryCards()}
-          <button
-            className='save-list-button'
-            onClick={submitNewPackingList} 
-          >Save List
-          </button>
         </>
       )
     } else {
