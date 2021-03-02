4f5c36d78b26110f9aef6d81695c868181428a61
Alyssa Bull <alyssabull@gmail.com>
2021-03-02 14:04:29 -0700

    Open and close categories cypress test

--- cypress/integration/packing_list.spec.js
+++ cypress/integration/packing_list.spec.js
@@ -1,5 +1,5 @@
 describe('Get Started', () => {
-  beforeEach(() => {
+   it('should display the packing list', () => {
     cy.visit('https://packsmart.herokuapp.com/')
     .get('.start-button').click()
     .get('input:first').type('Spring Break')
@@ -11,10 +11,21 @@ describe('Get Started', () => {
     .get('[name="Beach"]').click()
     .get('[name="NightLife"]').click()
     .get('.quiz-submit-btn').click()
-  })
-  
-  it('should display the packing list', () => {
     cy.contains('Spring Break')
+  })
+
+  it('should be able to open categories', () => {
+    cy.get(':nth-child(2) > .arrow-container > .edit-category-btn > svg').click()
+    .get(':nth-child(3) > .arrow-container > .edit-category-btn > svg').click()
+    .get(':nth-child(4) > .arrow-container > .edit-category-btn > svg').click()
+    .get(':nth-child(5) > .arrow-container > .edit-category-btn > svg').click()
+  })
+
+  it('should be able to edit the quantity of items in multiple categories', () => {
+    cy.get(':nth-child(2) > .list-items > :nth-child(1) > .quantity-container > :nth-child(3)').click()
+    .get(':nth-child(2) > .list-items > :nth-child(4) > .quantity-container > :nth-child(3)').click()
+    .get(':nth-child(3) > .list-items > :nth-child(1) > .quantity-container > :nth-child(3)').click()
+    .get(':nth-child(3) > .list-items > :nth-child(1) > .quantity-container > :nth-child(1)').click()
   })
 
 })
