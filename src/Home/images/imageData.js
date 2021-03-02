6c80f664a2556b7e5499d1469a62507d63062c33
Alyssa Bull <alyssabull@gmail.com>
2021-03-02 08:42:34 -0700

    Add more background images

--- src/Home/images/imageData.js
+++ src/Home/images/imageData.js
@@ -14,10 +14,27 @@ import background12 from './background12.jpg'
 import background13 from './background13.jpg'
 import background14 from './background14.jpg'
 import background15 from './background15.jpg'
+import background16 from './background16.jpg'
+import background17 from './background17.jpg'
+import background18 from './background18.jpg'
+import background19 from './background19.jpg'
+import background20 from './background20.jpg'
+import background21 from './background21.jpg'
+import background22 from './background22.jpg'
+import background23 from './background23.jpg'
+import background24 from './background24.jpg'
+import background25 from './background25.jpg'
+import background26 from './background26.jpg'
+import background27 from './background27.jpg'
+import background28 from './background28.jpg'
 
 export const backgroundImages = [
   background0, background1, background2, background3, 
   background4, background5, background6, background7,
   background8, background9, background10, background11,
-  background12, background13, background14, background15
+  background12, background13, background14, background15,
+  background16, background17, background18, background19,
+  background20, background21, background22, background23, 
+  background24, background25, background26, background27,
+  background28
 ]
