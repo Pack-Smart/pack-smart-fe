ac855bd33850920c99abf4dc4d15599ddb68060e
Alyssa Bull <alyssabull@gmail.com>
2021-03-01 21:10:37 -0700

    Generate random background image

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,19 +1,25 @@
 import './Home.scss'
 import { Link } from 'react-router-dom'
-import lady from './lady.png'
+import { backgroundImages } from './images/imageData'
 
 const Home = () => {
   const generateRandomImage = () => {
-    let randomIndex = Math.floor(Math.random() * 49)
+    let randomIndex = Math.floor(Math.random() * 3)
+    return backgroundImages[randomIndex]
+  }
+
+  const backgroundStyle = {
+    backgroundImage: `url(${generateRandomImage()})`,
+    backgroundSize: 'cover'
   }
 
   return(
-    <div className='home-container'>
+    <div className='home-container' style={ backgroundStyle }>
       <section className='home-aside'></section>
       <section className='home'>
       <h1 className='pack-smart-title'>Pack Smart</h1>
-        <p>Having trouble figuring out what to pack for your next trip? Take our quiz to build the perfect list!</p>
-        <img src={ lady }/>
+        <p>Having trouble figuring out what to pack for your next trip?</p>
+        <p>Take our quiz to build the perfect list!</p>
         <Link to='/packing-quiz' className='start-button'>
           Get Started!
         </Link>
---
+++ src/Home/images/imageData.js
@@ -0,0 +1,8 @@
+import background0 from './background0.jpg'
+import background1 from './background1.jpg'
+import background2 from './background2.jpg'
+import background3 from './background3.jpg'
+
+export const backgroundImages = [
+  background0, background1, background2, background3
+]
