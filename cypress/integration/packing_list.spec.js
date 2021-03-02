87fa1ee7217489148070dfd0f9e59d2f1d45b58e
Alyssa Bull <alyssabull@gmail.com>
2021-03-02 14:10:45 -0700

    Delete item cypress test

--- cypress/integration/packing_list.spec.js
+++ cypress/integration/packing_list.spec.js
@@ -14,20 +14,27 @@ describe('Get Started', () => {
     cy.contains('Spring Break')
   })
 
-  it('should be able to open categories', () => {
+  it('should allow user to open categories', () => {
     cy.get(':nth-child(2) > .arrow-container > .edit-category-btn > svg').click()
     .get(':nth-child(3) > .arrow-container > .edit-category-btn > svg').click()
     .get(':nth-child(4) > .arrow-container > .edit-category-btn > svg').click()
     .get(':nth-child(5) > .arrow-container > .edit-category-btn > svg').click()
   })
 
-  it('should be able to edit the quantity of items in multiple categories', () => {
+  it('should allow user to edit the quantity of items in multiple categories', () => {
     cy.get(':nth-child(2) > .list-items > :nth-child(1) > .quantity-container > :nth-child(3)').click()
     .get(':nth-child(2) > .list-items > :nth-child(4) > .quantity-container > :nth-child(3)').click()
     .get(':nth-child(3) > .list-items > :nth-child(1) > .quantity-container > :nth-child(3)').click()
     .get(':nth-child(3) > .list-items > :nth-child(1) > .quantity-container > :nth-child(1)').click()
     .get(':nth-child(4) > .list-items > :nth-child(2) > .quantity-container > :nth-child(3)').click()
     .get(':nth-child(4) > .list-items > :nth-child(3) > .quantity-container > :nth-child(3)').click()
+  })
+
+  it('should allow user to delete item', () => {
+    cy.get(':nth-child(2) > .list-items > :nth-child(6) > .delete > .delete-item').click()
+    .get('.modal-buttons > :nth-child(1)').click()
+    .get(':nth-child(3) > .list-items > :nth-child(2) > .delete > .delete-item').click()
+    .get('.modal-buttons > :nth-child(1)').click()
   })
 
 })
