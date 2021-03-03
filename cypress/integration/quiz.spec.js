aab79989f714a5bacd35e20e71cdc0465c3942e7
Kara Caputo <kncaputo@gmail.com>
2021-03-02 20:54:27 -0500

    Clean up quiz.spec

--- cypress/integration/quiz.spec.js
+++ cypress/integration/quiz.spec.js
@@ -1,18 +1,5 @@
 describe('Get Started', () => {
   beforeEach(() => {
-  //   cy.fixture('getPackingListData.json')
-  //     .then((getPackingListData) => {cy.intercept('POST', 'http://localhost:3000/', {
-  //       statusCode: 200,
-  //       body: getPackingListData
-  //     })
-  //   })
-
-  //     cy.fixture('saveNewPackingList.json')
-  //     .then((saveNewPackingList) => {cy.intercept('POST', 'http://localhost:3000/', {
-  //       statusCode: 200,
-  //       body: saveNewPackingList
-  //     })
-  //   })
     cy.visit('http://localhost:3000/').wait(1000)
       .get('.start-button').click()
       .get('[name="Cool"]').click()
@@ -46,8 +33,6 @@ describe('Get Started', () => {
 
   it.only('user will see their packing list if all required buttons are clicked', () => {
     cy.get('[name="Male Typical"]').click()
-      // .get('[name="Cool"]').click()
-      // .get('[name="Warm"]').click()
       .get('input:first').type('Dream Team Test')
       .get('input').eq(1).type('Will\'s House')
       .get('input:last').type('123456')
@@ -79,39 +64,3 @@ describe('Get Started', () => {
       .url().should('include', '/packing-list')
     })
 })
-  
-/*
-Quiz: All quiz.spec.js
-- click on Get Started
-- Take the quiz (required sad path)
-- click 'Get My Packing List!'
-
-
-Edit Packing List: Alyssa edit_packing_list.spec.js
-- click ^ and down carrot
-- change quantity
-- click checked
-- click delete 'X'
-    - click 'take me back'
-    - click 'please dont ...'
-    - click 'delete'
-
-Edit Title: JP edit_trip_details.spec.js
-- click edit pencil
-- change trip details (sad path for '')
-- click 'Save'
-
-
-Headers: Kara headers.spec.js
-- click 'How It Works'
-- click 'Start New List'
-- click 'Saved Lists'
-- click 'PS'
-
-Saved Lists: Sarah saved_list.spec.js
-- click delete trip
-- click trip
-
-Sad path for api calls:
-(We have a lot ... and I'm going to lunch right now. ;) )
-*/
