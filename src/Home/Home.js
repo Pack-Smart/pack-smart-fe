6e4078e8b34d9df43f6fe6d9e35b82a90ddad189
Kara Caputo <kncaputo@gmail.com>
2021-03-03 16:43:39 -0500

    Remove negative margin

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,18 +5,17 @@ import { backgroundStyle } from './backgroundStyle'
 const Home = () => {
   return(
     <div className='home-container' style={ backgroundStyle }>
-      <section className='home-aside'></section>
       <section className='home'>
-      <h1 className='pack-smart-title'>Pack Smart</h1>
-      <section className='pack-smart-description'>
-        <h2>Having trouble figuring out what to pack for your next trip?</h2>
-        <h2>Let Pack Smart curate your perfect list!</h2>
+        <h1 className='pack-smart-title'>Pack Smart</h1>
+        <section className='pack-smart-description'>
+          <h2>Having trouble figuring out what to pack for your next trip?</h2>
+          <h2>Let Pack Smart curate your perfect list!</h2>
+          <Link to='/packing-quiz' className='start-button'>
+            Get Started!
+          </Link>
+        </section>
+
       </section>
-        <Link to='/packing-quiz' className='start-button'>
-          Get Started!
-        </Link>
-      </section>
-      <section className='home-aside'></section>
     </div>
   )
 }
